b1c50bd2ae441a7d921ff2a33aef642516e1b079
Zawad Mutawassit <zawadmann@gmail.com>
2025-03-27 11:44:11 -0230

    minor bugs fix

--- closestToLeft.cpp
+++ closestToLeft.cpp
@@ -3,23 +3,26 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main() 
+{
     int n, k;
     cin >> n >> k;  // n = number of elements, k = number of queries
 
     vector<long long> arr(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cin >> arr[i];
     }
 
     sort(arr.begin(),arr.end());
 
-    while (k--) {
+    while (k--) 
+    {
         long long x;
         cin >> x;
 
-        int answer = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
-        cout << answer << "\n";
+        int ans = upper_bound(arr.begin(), arr.end(), x) - arr.begin();
+        cout << ans << "\n";
     }
     return 0;
 }
